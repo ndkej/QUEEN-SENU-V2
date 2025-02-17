ac5251dff32c880a9b12fff79a2d1d789160581a
ndkej <nimeshasudeshani402@gmail.com>
2025-02-17 19:05:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'KIgknJqS#ta_8waLqM5V35eIYeX1Cl7xk4PA0fRaJSQ5BjYZVCDk',
+    SESSION_ID: process.env.SESSION_ID || '7YpwEQIL#QgMyikLRpyBNxSfM70MirTRrqDacUgjyG5gz7UoDTjw',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 };
